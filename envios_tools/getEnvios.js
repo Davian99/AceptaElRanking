dfa9cadeb75390544dc6cf856402608c108dc950
Davian99 <davidanimal1999@hotmail.com>
2021-12-18 13:45:46 +0100

    Reestructure folder syste

---
+++ envios_tools/getEnvios.js
@@ -0,0 +1,49 @@
+const fetch = require('isomorphic-fetch');
+const fs = require('fs');
+const { parseEnvio } = require('./parsers');
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+
+const connectDB = async (db_name) => {
+    const client = new MongoClient(url);
+    await client.connect();
+    const db = client.db(db_name);
+    return [client, db];
+}; 
+
+const fetchEnvio = n_envio => {
+    return fetch(`https://www.aceptaelreto.com/user/submission.php?id=${n_envio}`, {
+        "credentials": "include",
+        "referrer": "https://www.aceptaelreto.com/user/lastsubmissions.php",
+        "method": "GET",
+        "mode": "cors"
+    }).then(res => res.text())
+}
+
+const getStoreEnvio = async (num_envio, db) => {
+    console.log(num_envio)
+    const envio = await fetchEnvio(num_envio);
+    const data = parseEnvio(envio);
+    console.log(data);
+    //await db.insertOne(data);
+};
+
+const getAll = async (ini, n, db) => {
+    const data = await Promise.all([...Array(n).keys()]
+        .map(elem => elem + ini)
+        .map(envio => fetchEnvio(envio)
+            .then(fetched => parseEnvio(fetched))
+        ));
+    console.log(data);
+};
+
+const main = async () => {
+    console.time('main');
+    const [client, db] = await connectDB('acepta_el_ranking');
+    const envios_collection = db.collection('envios');
+    await getStoreEnvio(484);
+    console.timeEnd('main');
+    client.close();
+}
+main();
---
+++ src/storeEnvios.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+
+const connectDB = async (db_name) => {
+    const client = new MongoClient(url);
+    await client.connect();
+    const db = client.db(db_name);
+    return [client, db];
+}; 
+
+const main = async () => {
+    const [client, db] = await connectDB('acepta_el_ranking');
+    const envios_collection = db.collection('envios');
+    
+    const envios = JSON.parse(fs.readFileSync('envios.json'))
+        .map(envio => ({
+            user: envio.NOMBRE,
+            num_envio: envio.NUM,
+            fecha: envio.FECHA,
+            problem: envio.PROBLEMA,
+            language: envio.LENGUAJE,
+            veredict: envio.VEREDICTO,
+            time: envio.TIEMPO,
+            memory: envio.MEMORIA,
+            position: envio.POSICION,
+        }));
+    envios.sort((a, b) => a.num_envio - b.num_envio);
+    await envios_collection.insertMany(envios);
+    console.log(envios[0]);
+    
+    client.close();
+}
+main();
